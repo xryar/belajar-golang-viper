3b4e06657ba27362ea816351a166a615a68786b9
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-06-18 09:12:37 +0700

    env configuration

--- viper_test.go
+++ viper_test.go
@@ -42,3 +42,18 @@ func TestYAML(t *testing.T) {
 	assert.Equal(t, 3306, config.GetInt("database.port"))
 	assert.Equal(t, true, config.GetBool("database.show_sql"))
 }
+
+func TestENV(t *testing.T) {
+	config := viper.New()
+	config.SetConfigFile("config.env")
+	config.AddConfigPath(".")
+
+	err := config.ReadInConfig()
+	assert.Nil(t, err)
+
+	assert.Equal(t, "belajar-golang-viper", config.GetString("APP_NAME"))
+	assert.Equal(t, "Arya Rizki Andaru", config.GetString("APP_AUTHOR"))
+	assert.Equal(t, "localhost", config.GetString("DATABASE_HOST"))
+	assert.Equal(t, 3306, config.GetInt("DATABASE_PORT"))
+	assert.Equal(t, true, config.GetBool("DATABASE_SHOW_SQL"))
+}
