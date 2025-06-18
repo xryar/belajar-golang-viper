7a13b456b5675f55c5d18ab921806bd830fe7b6c
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-06-18 09:00:23 +0700

    first commit

---
+++ viper_test.go
@@ -0,0 +1,29 @@
+package belajargolangviper
+
+import (
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestViper(t *testing.T) {
+	config := viper.New()
+	assert.NotNil(t, config)
+}
+
+func TestJSON(t *testing.T) {
+	config := viper.New()
+	config.SetConfigName("config")
+	config.SetConfigType("json")
+	config.AddConfigPath(".")
+
+	err := config.ReadInConfig()
+	assert.Nil(t, err)
+
+	assert.Equal(t, "belajar-golang-viper", config.GetString("app.name"))
+	assert.Equal(t, "Arya Rizki Andaru", config.GetString("app.author"))
+	assert.Equal(t, "localhost", config.GetString("database.host"))
+	assert.Equal(t, 3306, config.GetInt("database.port"))
+	assert.Equal(t, true, config.GetBool("database.show_sql"))
+}
